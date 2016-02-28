55bf0f8212660caa32748d3628c9d34ae087c802
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2016-02-28 13:05:34 +0000

    playing about

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const lsRoutes = require('express-ls-routes');
+const app = express();
+
+app.set('view engine', 'jade');
+
+
+app.get("/text", lsRoutes(app), (req, res) => {
+  res.render("text")
+});
+
+app.listen("3000");
